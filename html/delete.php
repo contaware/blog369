4952d730780924ec02d77b4103a5e64017a9afd2
Oliver Pfister <oliver.pfister@contaware.com>
2026-03-12 17:25:03 +0100

    Add can_change_feedback() function

--- html/delete.php
+++ html/delete.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/inc/configuration.php';
 require_once __DIR__ . '/inc/auth.php';
 require_once __DIR__ . '/inc/database.php';
+
 if (isset($_GET['id'])) {
     // Check the given id
     $id = $_GET['id'];
@@ -9,20 +10,8 @@ if (isset($_GET['id'])) {
         die("Error: a numeric id is required");
 
     // Make sure we are allowed to delete the given id
-    try {
-        $sql = "SELECT user_id FROM feedback WHERE id = ?";
-        $res = $conn->prepare($sql);
-        $res->bindValue(1, (int)$id, PDO::PARAM_INT);
-        $res->execute();
-        $user_id = $res->fetchColumn();
-        if ($user_id === false) // use === because a 0 int is a valid id
-            die("Error: could not find the entry with id=$id");
-        if (!isCurrentUser($user_id) && !isAdmin())
-            die("Error: not allowed to delete the entry with id=$id");
-    }
-    catch (Throwable $e) {
-        die("PDO failed: {$e->getMessage()}\n");
-    }
+    if (($ret = can_change_feedback($id)) !== true)
+        die($ret);
 
     // Delete the given id
     try {
--- html/inc/database.php
+++ html/inc/database.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/configuration.php';
+require_once __DIR__ . '/auth.php';
+
 try {
     $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
     $conn = new PDO($dsn, DB_USER, DB_PASS);
@@ -7,6 +9,27 @@ try {
 catch (Throwable $e) {
     die("PDO failed: {$e->getMessage()}\n");
 }
+
 function db_maintenance_link($e) {
     return "PDO failed: {$e->getMessage()}\n<br><a href=\"maintenance.php\">Go to Maintenance</a> page to solve the problem.\n";
+}
+
+function can_change_feedback($id) {
+    global $conn;
+    
+    try {
+        $sql = "SELECT user_id FROM feedback WHERE id = ?";
+        $res = $conn->prepare($sql);
+        $res->bindValue(1, (int)$id, PDO::PARAM_INT);
+        $res->execute();
+        $user_id = $res->fetchColumn();
+        if ($user_id === false) // use === because a 0 int is a valid id
+            return "Error: could not find the entry with id=$id";
+        if (!isCurrentUser($user_id) && !isAdmin())
+            return "Error: not allowed to change the entry with id=$id";
+        return true;
+    }
+    catch (Throwable $e) {
+        die("PDO failed: {$e->getMessage()}\n");
+    }
 }
